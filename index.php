4dadbc9a76240175bd8896045f295ea2cf364227
Antonio Vivas <antoniovivascavalcante@gmail.com>
2021-10-12 12:49:07 +0200

    Exercicio Herança

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
     <pre>
         <?php
-            class pessoa{
+            abstract class Pessoa{
                 private $nome;
                 private $idade;
                 private $sexo;
@@ -37,103 +37,92 @@
                 private function setSexo($s){
                     $this->sexo = $s;
                 }
-                public function fazerAniversario(){
+                final public function fazerAniversario(){
                     $this->setIdade($this->getIdade() + 1);
                 }
             }
-            interface publicacao{
-                public function abrir();
-                public function fechar();
-                public function folhear($p);
-                public function avancarPag();
-                public function voltarPag();
-            }
-            class livro implements publicacao{
-                private $titulo;
-                private $autor;
-                private $totPaginas;
-                private $pagAtual;
-                private $aberto;
-                private $leitor;
-                function __construct($titulo, $autor, $totPaginas, $leitor)
-                {
-                    $this->titulo = $titulo;
-                    $this->autor = $autor;
-                    $this->totPaginas = $totPaginas;
-                    $this->aberto = false;
-                    $this->pagAtual = 0;
-                    $this->leitor = $leitor;
+            class Aluno extends Pessoa{
+                private $matricula;
+                private $curso;
+                function getMatricula(){
+                    return $this->matricula;
                 }
-                public function getTitulo(){
-                    return $this->titulo;
+                function setMatricula($m){
+                    $this->matricula = $m;
                 }
-                private function setTitulo($t){
-                    $this->titulo = $t;
+                function getCurso(){
+                    return $this->curso;
                 }
-                public function getAutor(){
-                    return $this->autor;
+                function setCurso($c){
+                    $this->curso = $c;
                 }
-                private function setAutor($a){
-                    $this->autor = $a;
+                function cancelarMatricula(){
+                    $this->setMatricula(false);
+                    echo"Matricula será cancelada";
                 }
-                public function getTotPaginas(){
-                    return $this->totPaginas;
-                }
-                private function setTotPaginas($p){
-                    $this->totPaginas = $p;
-                }
-                public function getPagAtual(){
-                    return $this->pagAtual;
-                }
-                public function setPagAtual($p){
-                    if($p > $this->getTotPaginas()){
-                        echo"O livro vai até a página ".$this->getTotPaginas();
-                    }else{
-                        $this->pagAtual = $p;
-                    }
-                }
-                public function getAberto(){
-                    return $this->aberto;
-                }
-                public function setAberto($boolean){
-                    $this->aberto = $boolean;
-                }
-                public function getLeitor(){
-                    return $this->leitor;
-                }
-                public function setLeitor($l){
-                    $this->leitor = $l;
-                }
-                public function abrir(){
-                    $this->setAberto(true);
-                }
-                public function fechar(){
-                    $this->setAberto(false);
-                }
-                public function folhear($p){
-                    $this->setPagAtual($p);
-                }
-                public function avancarPag(){
-                    $this->setPagAtual($this->getPagAtual() + 1);
-                }
-                public function voltarPag(){
-                    $this->setPagAtual($this->getPagAtual() - 1);
-                }
-                public function detalhes(){
-                    echo "Livro".$this->getTitulo()." escrito por ".$this->getAutor();
-                    echo "<br> Páginas: ".$this->getTotPaginas()." atual ".$this->getPagAtual();
-                    echo "<br> Sendo lido por ".$this->leitor->getNome();
+                function pagarMensalidade(){
+                    echo "Mensalidade foi paga!";
                 }
             }
-            $p = array();
-            $p[0] = new pessoa("Tonio", 20, "Masculino");
-            $p[1] = new pessoa("Caça Rato", 20, "Masculino");
-            $l = array();
-            $l[0] = new livro("Titulo do livro 1", "Euzinho", 100, $p[0]);
-            $l[1] = new livro("Titulo do livro 2", "outra pessoa", 75, $p[1]);
-            $l[0]->folhear(15);
-            $l[0]->avancarPag();
-            $l[0]->detalhes();  
+            class Professor extends Pessoa{
+                private $especialidade;
+                private $salario;
+                function getEspecialidade(){
+                    return $this->especialidade;
+                }
+                function setEspecialidade($e){
+                    $this->especialidade = $e;
+                }
+                function getSalario(){
+                    return $this->salario;
+                }
+                function setSalario($s){
+                    $this->salario = $s;
+                }
+                function receberAumento($aum){
+                    $this->salario = $this->getSalario() + $aum;
+                }
+            }
+            final class Funcionario extends Pessoa{
+                private $setor;
+                private $trabalhando;
+                function getSetor(){
+                    return $this->setor;
+                }
+                function setSetor($s){
+                    $this->setor = $s;
+                }
+                function getTrabalhando(){
+                    return $this->trabalhando;
+                }
+                function setTrabalhando($boolean){
+                    $this->trabalhando = $boolean;
+                }
+                function mudarTrabalho($boolean){
+                    $this->trabalhando = $boolean;
+                }
+            }
+            class Bolsista extends Aluno{
+                private $bolsa;
+                function getBolsa(){
+                    return $this->bolsa;
+                }
+                function setBolsa($b){
+                    $this->bolsa = $b;
+                }
+                function renovarBolsa(){
+                    echo"Bolsa renovada";
+                }
+                function pagarMensalidade(){
+                    echo "O aluno ".$this->getNome()." é bolsista, então paga com desconto";
+                }
+            }
+            $p2 = new Aluno("Kamilla", 21, "Feminino");
+            $p3 = new Professor("Caça rato", 20, "Home");
+            $p4 = new Funcionario("Yasmine", 23, "Mulé");
+            $p2->setMatricula(1234);
+            print_r($p1);
+            print_r($p2);
         ?>
     </pre>
 </body>
